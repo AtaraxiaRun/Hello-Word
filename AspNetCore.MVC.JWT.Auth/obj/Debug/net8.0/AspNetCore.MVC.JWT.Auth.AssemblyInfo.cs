79132b7a5978870d04ea7990d3f1db3dbe132dc7
Wangrui <12988775182@qq.com>
2024-02-28 16:50:19 +0800

    更改Filters文件名称

--- AspNetCore.MVC.JWT.Auth/obj/Debug/net8.0/AspNetCore.MVC.JWT.Auth.AssemblyInfo.cs
+++ AspNetCore.MVC.JWT.Auth/obj/Debug/net8.0/AspNetCore.MVC.JWT.Auth.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCore.MVC.JWT.Auth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cb297abdb111b2ca8ebfe9710c832aa4aa35cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCore.MVC.JWT.Auth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCore.MVC.JWT.Auth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
